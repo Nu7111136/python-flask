4b3b24e97b16d95a6a9cd87190691373a331e50b
Nouman <nouman.zahid@afiniti.com>
2025-12-05 21:09:15 +0500

    my new flaks calculator method

--- FlaskCalculator.py
+++ FlaskCalculator.py
@@ -12,7 +12,7 @@ class Calculator:
 app = Flask(__name__)
 
 @app.route('/add', methods=['GET'])
-def add_numbers():
+def add_numbers_new_python():
     a = int(request.args.get('a', 0))
     b = int(request.args.get('b', 0))
     result = Calculator.add(a, b)
